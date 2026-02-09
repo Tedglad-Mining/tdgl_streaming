a3e7bf6f0068ad324dd6c7466cb27e72dc51bc26
Percival <percival.rapha@gmail.com>
2026-02-09 17:00:15 +0200

    sync base

---
+++ tdgl_streaming/tdgl_streaming/doctype/sync_config/test_sync_config.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Percival Rapha and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSyncConfig(FrappeTestCase):
+	pass
---
+++ tdgl_streaming/tdgl_streaming/doctype/sync_local_edit/sync_local_edit.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Percival Rapha and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sync Local Edit', {
+	// refresh: function(frm) {
+
+	// }
+});
